ccb17ec2dfdadea7539c01ced7cf0fa14cfebd12
LucasDesmery <ldesmery@itba.edu.ar>
2025-05-31 16:08:08 -0300

    PongisUtils update

--- Userland/Wii Pong resort/PongisUtils.h
+++ Userland/Wii Pong resort/PongisUtils.h
@@ -1,6 +1,20 @@
 #ifndef PONGIS_UTILS_H
 #define PONGIS_UTILS_H
-#include <Pongis.h>
+
+#include <stdint.h>
+typedef struct {
+    uint64_t x;
+    uint64_t y;
+    uint64_t acceleration_x;
+    uint64_t acceleration_y;
+    uint64_t vel_x;
+    uint64_t vel_y;
+    float angle;
+    uint64_t max_speed;
+    uint64_t max_acceleration;
+    int x_offset;
+    int y_offset;
+} body;
 
 void sincosf(float angle, float *s, float *c);
 void arctan(double x, int terms, float *angle);
