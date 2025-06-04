48da1513592532eeb4ab5adb415ebdf4cdfe0978
LucasDesmery <ldesmery@itba.edu.ar>
2025-06-04 12:47:52 -0300

    Pongis screenUpdate and 2nd pla

--- Userland/shell/include/pongisUtils.h
+++ Userland/shell/include/pongisUtils.h
@@ -2,18 +2,20 @@
 #define PONGIS_UTILS_H
 
 #include <stdint.h>
+#include <stdlib.h>
 #define PI 3.1415926f
+#define OFFSET 10
 typedef struct {
     uint64_t x;
     uint64_t y;
     float vel_x;
     float vel_y;
     int rotation;
-    uint64_t max_speed;
-    int x_offset;
-    int y_offset;
 } body;
-
+extern uint64_t hole_x;
+extern uint64_t hole_y;
+extern char end;
+extern int flag;
 void sincosf(float angle, float *s, float *c);
 void arctan(double x, int terms, float *angle);
 void applyForces(body *b, float angle, float magnitud);
