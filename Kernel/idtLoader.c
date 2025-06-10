19732c5cc0229b0c6f3d89993c6a59c917762bf3
Bautista Simone <bsimone@itba.edu.ar>
2025-06-10 20:33:29 -0300

    style: changed languages

--- Kernel/idtLoader.c
+++ Kernel/idtLoader.c
@@ -1,9 +1,9 @@
 #include <idtLoader.h>
 
-#pragma pack(push)		/* Push de la alineación actual */
-#pragma pack (1) 		/* Alinear las siguiente estructuras a 1 byte */
+#pragma pack(push)		/* Push the current alignment */
+#pragma pack (1) 		  /* Align the following structures to 1 byte */
 
-/* Descriptor de interrupcion */
+/* Interrupt descriptor */
 typedef struct {
     uint16_t offset_l, selector;
     uint8_t cero, access;
@@ -11,9 +11,9 @@ typedef struct {
   	uint32_t offset_h, other_cero;
 } DESCR_INT;
 
-#pragma pack(pop)		/* Reestablece la alinceación actual */
+#pragma pack(pop)		/* Restore the current alignment */
 
-DESCR_INT * idt = (DESCR_INT *) 0;	// IDT de 255 entradas
+DESCR_INT * idt = (DESCR_INT *) 0;	IDT with 255 entries
 
 static void setup_IDT_entry (int index, uint64_t offset) {
   idt[index].selector = 0x08;
