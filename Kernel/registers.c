01683aed7955b011f9f9ca70a28383998efa89da
Jaime Nazar Anchorena <jnazaranchorena@itba.edu.ar>
2025-06-16 18:15:05 -0300

    fix?

--- Kernel/registers.c
+++ Kernel/registers.c
@@ -4,8 +4,7 @@ static uint64_t generalRegs[GENERAL_REGISTERS_COUNT];
 static uint64_t specialRegs[SPECIAL_REGISTERS_COUNT];
 
 const char * generalRegisterString[] = {                               //every entry with a lenght of 8
-	"RAX:    ", "RBX:    ", "RCX:    ", "RDX:    ",
-	"RBP:    ", "RDI:    ", "RSI:    ",
+	"RAX:    ", "RBX:    ", "RCX:    ", "RDX:    ", "RSI:    ", "RDI:    ", "RSP:    ", "RBP:    ",
 	"R8:     ", "R9:     ", "R10:     ", "R11:    ", "R12:    ", "R13:    ", "R14:    ", "R15:    "
 };
 
